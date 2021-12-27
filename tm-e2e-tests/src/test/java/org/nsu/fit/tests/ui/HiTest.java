ba4a30f518d517bb4f3db5935f332f6df2a90f81
pchagina <polina.chagina@betoola.com>
2021-12-27 18:25:18 +0700

    Update threads

--- tm-e2e-tests/src/test/java/org/nsu/fit/tests/ui/HiTest.java
+++ tm-e2e-tests/src/test/java/org/nsu/fit/tests/ui/HiTest.java
@@ -9,10 +9,35 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HiTest extends BaseTest {
-    @Test(description = "Greetings via UI.")
+    @Test(description = "Greetings admin via UI.")
     @Severity(SeverityLevel.BLOCKER)
     @Feature("Auth feature.")
     public void hiAdminTest() {
+        AdminScreen screen = new LoginScreen(browser)
+                .loginAsAdmin();
+
+        String greetingsAdmin = screen.getGreetings();
+
+        Assert.assertEquals(greetingsAdmin, "Hi admin!");
+
+    }
+
+    @Test(description = "Greetings customer via UI.")
+    @Severity(SeverityLevel.BLOCKER)
+    @Feature("Auth feature.")
+    public void hiCustomerTest() {
+
+        String greetingsCustomer = new LoginScreen(browser)
+                .loginAsCustomer("test@test.com", "testtest")
+                .getGreetings();
+
+        Assert.assertEquals(greetingsCustomer, "Hi Test Test!");
+    }
+
+    @Test(description = "Greetings different users via UI.")
+    @Severity(SeverityLevel.BLOCKER)
+    @Feature("Auth feature.")
+    public void hiAnyoneTest() {
         AdminScreen screen = new LoginScreen(browser)
                 .loginAsAdmin();
 
